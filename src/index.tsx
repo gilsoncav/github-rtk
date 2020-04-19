d51530fac78c2839755eb7b968f4c199b61582cc
Gilson Cavalcanti <gilsoncav@gmail.com>
2020-04-19 11:26:55 -0300

    Inserted the Provider and configured webpack HMR

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './app/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import store from 'app/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const render = () => {
+  // importing dynamically and asynchronally as the module.hot.accept triggers
+  // the render callback that we setted bellow
+  const App = require('./app/App').default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+render();
+
+// we set a async trigger to call the render() callback using
+// webpack's Hot Module Replacement
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('app/App', render);
+}
